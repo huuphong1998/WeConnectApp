0028ffdb6e8a4e67f1a3f5afdc7a2551d1142b23
Huu Phong <nghuuphong1998@gmail.com>
2025-09-15 14:51:28 +0700

    Configure RTK Query and create registration endpoint

--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,20 +1,33 @@
 import FormField from '@components/FormField';
 import TextInput from '@components/FormInputs/TextInput';
-import { Button } from '@mui/material';
+import { Alert, Button } from '@mui/material';
+import { useRegisterMutation } from '@services/rootApi';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
-    const { control } = useForm();
+    const { control, handleSubmit } = useForm();
+
+    const [register, { data, isLoading, error, isError }] = useRegisterMutation();
+
+    function onSubmit(formData) {
+        console.log({ formData });
+        register(formData);
+    }
+
+    console.log({ data, isLoading, error });
 
     return (
         <div>
             <p className="text-dark-100 mb-5 text-center text-2xl font-bold">Register</p>
-            <form className="flex flex-col gap-4">
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
                 <FormField name="fullName" label="Full Name" control={control} Component={TextInput} />
                 <FormField name="email" label="Email" control={control} Component={TextInput} />
                 <FormField name="password" label="Password" type="password" control={control} Component={TextInput} />
-                <Button variant="contained">Sign up</Button>
+                <Button variant="contained" type="submit">
+                    Sign up
+                </Button>
+                {isError && <Alert severity="error">{error?.data?.message}</Alert>}
             </form>
             <p className="mt-4">
                 Already have an account?{' '}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '@redux/slices/authSlice';
+import { rootApi } from '@services/rootApi';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        [rootApi.reducerPath]: rootApi.reducer,
+    },
+    middleware: getDefaultMiddleware => {
+        return getDefaultMiddleware().concat(rootApi.middleware);
     },
 });

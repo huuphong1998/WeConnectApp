1ed3525fabc56aea8e0c6814a99f5c58c6f89827
Huu Phong <nghuuphong1998@gmail.com>
2025-09-15 11:37:19 +0700

    redux toolkit configuration

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,8 @@ import RegisterPage from '@pages/auth/RegisterPage';
 import AuthLayout from '@pages/auth/AuthLayout';
 import LoginPage from '@pages/auth/LoginPage';
 import OTPVerifyPage from '@pages/auth/OTPVerifyPage';
+import { Provider } from 'react-redux';
+import { store } from '@redux/store';
 
 const router = createBrowserRouter([
     {
@@ -42,9 +44,11 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById('root')).render(
-    <ThemeProvider theme={theme}>
-        <ModalProvider>
-            <RouterProvider router={router} />
-        </ModalProvider>
-    </ThemeProvider>
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <ModalProvider>
+                <RouterProvider router={router} />
+            </ModalProvider>
+        </ThemeProvider>
+    </Provider>
 );
